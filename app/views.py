c83188fb434726d4a1e56dd9f9ffd52ac8b7c703
alchermd <johnalcherdoloiras@gmail.com>
2017-05-16 16:51:33 +0800

    Render a template for a GET request on the registration page.
    MySQL should be setup before building the logic for a POST request.

--- app/views.py
+++ app/views.py
@@ -1,6 +1,14 @@
-from flask import render_template
+from flask import render_template, request
+from app.forms import RegistrationForm
 from app import app
 
 @app.route("/")
 def index():
     return render_template("home.html", title="Home")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm(request.form)
+    if request.method == "POST" and form.validate():
+        pass
+    return render_template("register.html", title="Register", form=form)
