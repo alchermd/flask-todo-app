5ce55d1d5c5edc3617af8c2c742f0d0251c6eb37
alchermd <johnalcherdoloiras@gmail.com>
2017-05-16 20:41:29 +0800

    Insert the user's credential into the database when sending a POST request on the register page.
    Several functions are imported for connecting to the database, password encryption, and flashing and redirecting.

--- app/views.py
+++ app/views.py
@@ -1,6 +1,11 @@
-from flask import render_template, request
+from flask import render_template, request, flash, redirect, url_for
+from flask_mysqldb import MySQL
+from passlib.hash import sha256_crypt
 from app.forms import RegistrationForm
 from app import app
+
+# MySQL connection.
+mysql = MySQL(app)
 
 @app.route("/")
 def index():
@@ -10,5 +15,22 @@ def index():
 def register():
     form = RegistrationForm(request.form)
     if request.method == "POST" and form.validate():
-        pass
+        # Get user data from form.
+        name = form.name.data
+        username = form.username.data
+        email = form.email.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        # Create a cursor and insert user into the database.
+        curr = mysql.connection.cursor()
+        curr.execute("INSERT INTO users(name, username, email, password) VALUES(%s, %s, %s, %s)", (name, username, email, password))
+
+        # Close connection.
+        mysql.connection.commit()
+        curr.close()
+
+        # Redirect the user.
+        flash("Account created!", "success")    
+        return redirect(url_for("login"))
+        
     return render_template("register.html", title="Register", form=form)
