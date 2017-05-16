0518e637dce951af0b420e36ffa233250d332e12
alchermd <johnalcherdoloiras@gmail.com>
2017-05-16 21:18:11 +0800

    Handle GET and POST requests on the login page.
    The user input is validated agains the database query (using the username). Passing validation will flash and redirect the user to the dashboard page. Failing validation will display an error on the page.

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, flash, redirect, url_for
+from flask import render_template, request, flash, redirect, url_for, session
 from flask_mysqldb import MySQL
 from passlib.hash import sha256_crypt
 from app.forms import RegistrationForm
@@ -34,3 +34,34 @@ def register():
         return redirect(url_for("login"))
         
     return render_template("register.html", title="Register", form=form)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # Get user data from form
+        username = request.form['username']
+        plain_pw = request.form['password']
+        
+        # Create a cursor and query the database.
+        curr = mysql.connection.cursor()
+        result = curr.execute("SELECT * FROM users WHERE username=%s", (username,))
+
+        # Check for match.
+        if result:
+            # Save user data from query result.
+            user_data = curr.fetchone()
+            hashed_pw = user_data['password']
+
+            # Validate password
+            if sha256_crypt.verify(plain_pw, hashed_pw):
+                # Save user session.
+                session['logged_in'] = True
+                session['username'] = user_data['username']
+                
+                # Redirect to dashboard.
+                flash("Login success.", "success")
+                return redirect(url_for("dashboard"))
+
+        return render_template("login.html", title="Login", err="Invalid credentials.")
+
+    return render_template("login.html", title="Login")
