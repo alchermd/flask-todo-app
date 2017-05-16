c29b82ee2ce1d16d60fcd4715d2836c7a6f2d241
alchermd <johnalcherdoloiras@gmail.com>
2017-05-16 20:33:06 +0800

    Set the MySQL related configuration.

--- app/__init__.py
+++ app/__init__.py
@@ -2,3 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 from app import views
+
+# Flask configuration
+app.config.from_object(__name__)
+app.config.update(dict(
+    MYSQL_HOST='localhost',
+    MYSQL_USER='root',
+    MYSQL_PASSWORD='123456',
+    MYSQL_DB='flask_todo_app',
+    MYSQL_CURSORCLASS='DictCursor',
+    SECRET_KEY='development key'
+))
